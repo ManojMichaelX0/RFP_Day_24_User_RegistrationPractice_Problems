75955dc23b3dc164c185fb76f8870e00c11bdf67
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-26 23:47:46 +0530

    added UC 12 Custom Exception

--- UserRegistrationDay26/Program.cs
+++ UserRegistrationDay26/Program.cs
@@ -3,126 +3,155 @@ using System.Text.RegularExpressions;
 
 namespace UserRegistrationDay26
 {
-    internal class Program
+    public class Program : Exception
     {
+        public static string firstName, lastName, email, mobileNumber, password;
+        public enum ExceptionType
+        {
+            Invalid_FirstName, Invalid_LastName, Invalid_Email, Invalid_MobileNumber, Invalid_Password
+        }
+        private readonly ExceptionType type;
         public static void Main(string[] args)
         {
             Console.WriteLine("User Registration");
-            Console.WriteLine("UC 1Enter First Name");
-            string firstName = Console.ReadLine();
-            Program p = new Program();
-
-            p.isMatchFirstName(firstName);
+            Console.WriteLine("UC 1 Enter First Name");
+            firstName = Console.ReadLine();
+            FirstNameCheck(firstName);
+            Console.WriteLine("UC 2 Enter Last Name");
+            lastName = Console.ReadLine();
+            LastNameCheck(lastName);
+            Console.WriteLine("UC 3 and 9 Enter Email");
+            email = Console.ReadLine();
+            EmailCheck(email);
+            Console.WriteLine("UC 4 Enter Mobile Number ");
+            mobileNumber = Console.ReadLine();
+            MobileNumberCheck(mobileNumber);
+            Console.WriteLine("UC 5 to 8 Enter Password");
+            password = Console.ReadLine();
+            PasswordCheck(password);
 
         }
         //UC 1 First Name
-        public string isMatchFirstName(string firstName)
+        public static bool FirstNameCheck(string fname)
         {
             try
             {
-                if (Regex.Match(firstName, "^[A-Z][a-z]{3,}$").Success)
+                var result = Regex.Match(fname, "^[A-Z][a-z]{3,}$").Success;
+                if (result)
                 {
-                    Console.WriteLine("Success , Firt Name is : " + firstName);
+                    Console.WriteLine("Success");
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("Failed");
-                    throw new Exception();
+                    return false;
                 }
             }
-            catch (Exception)
+            catch
             {
-                Console.WriteLine("First Name should Start with Upper Case Letter and minimum charecters should be 3 ");
+                throw new Program(Program.ExceptionType.Invalid_FirstName, "Invalid First Name");
             }
-            return firstName;
 
         }
+
         //UC 2 Last Name
-        public string isMatchLastName(string lastName)
+        public static bool LastNameCheck(string lname)
         {
             try
             {
-                if (Regex.Match(lastName, "^[A-Z][a-z]{3,}$").Success)
+                var result = Regex.Match(lname, "^[A-Z][a-z]{3,}$").Success;
+                if (result)
                 {
-                    Console.WriteLine("Success , Last Name is : " + lastName);
+                    Console.WriteLine("Success");
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("Failed");
-                    throw new Exception();
+                    return false;
                 }
             }
             catch (Exception)
             {
-                Console.WriteLine("Last Name should Start with Upper Case Letter and minimum charecters should be 3 ");
+                throw new Program(Program.ExceptionType.Invalid_FirstName, "Invalid Last Name");
             }
-            return lastName;
 
         }
         //UC 3 and 9 Email
-        public string isMatchEmail(string email)
+        public static bool EmailCheck(string mail)
         {
             try
             {
-                if (Regex.Match(email, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$").Success)
+                var result = Regex.Match(mail, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$").Success;
+                if (result)
                 {
-                    Console.WriteLine("Success , Email is : " + email);
+                    Console.WriteLine("Success");
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("Failed");
-                    throw new Exception();
+                    return false;
                 }
             }
             catch (Exception)
             {
-                Console.WriteLine("Email \nEx abc.xyz@bl.co.in\nEmail should have 3 mandatory Parts(abc,bl&co)with precise @ and . postions");
+                throw new Program(Program.ExceptionType.Invalid_FirstName, "Invalid Email");
             }
-            return email;
         }
         //UC 4 Mobile Number
-        public string isMatchMobileNumber(string mobileNumber)
+        public static bool MobileNumberCheck(string mobileNumber)
         {
             try
             {
-                if (Regex.Match(mobileNumber, @"^([0-9]{2})\s([0-9]{10})$").Success)
+                var result = Regex.Match(mobileNumber, @"^([0-9]{2})\s([0-9]{10})$").Success;
+                if (result)
                 {
-                    Console.WriteLine("Success , Mobile Number is : " + mobileNumber);
+                    Console.WriteLine("Success");
+                    return true;
                 }
                 else
                 {
                     Console.WriteLine("Failed");
-                    throw new Exception();
+                    return false;
                 }
             }
             catch (Exception)
             {
-                Console.WriteLine("Mobile Number Should have 2 number for Country code followed by space and 10 digit number ");
+                throw new Program(Program.ExceptionType.Invalid_FirstName, "Invalid Mobile Number");
             }
-            return mobileNumber;
 
         }
-        //UC 5,6,7,8 Password
-        public string isMatchPassword(string password)
+        //UC 5 Password
+        public static bool PasswordCheck(string password)
         {
             try
             {
-                if (Regex.Match(password, @"^([A-za-z]{5,})([@~!`#$%^&*]{1})([0-9]{1,})$").Success)
-                {
-                    Console.WriteLine("Success , Password is : " + password);
-                }
-                else
-                {
-                    Console.WriteLine("Failed");
-                    throw new Exception();
-                }
+            var result = Regex.Match(password, @"^([A-Za-z]{5,})([`~!@#$%^&*()_+=:;.,'<>/|\?]{1})([0-9]{1,})$").Success;
+            if (result)
+            {
+                Console.WriteLine("Success");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Failed");
+                return false;
+            }
             }
             catch (Exception)
             {
-                Console.WriteLine("Password Should have \n1)minimum 8 characters\n2)Atleast 1 UpperCase letter\n3)Atleast 1 number\n4)exactly 1 Special Charecter\n5)");
+                throw new Program(Program.ExceptionType.Invalid_FirstName, "Invalid PassWord");
             }
-            return password;
 
+        }
+
+        //UC 10
+
+        public Program(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
         }
     }
 
