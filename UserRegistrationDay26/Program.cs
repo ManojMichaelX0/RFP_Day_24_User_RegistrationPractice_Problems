c71751ca38b4191c8befcf1e6536c547ef8b92c3
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-26 21:58:45 +0530

    added User Registration Day 26 Problems

---
+++ UserRegistrationDay26/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationDay26
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration");
+            Console.WriteLine("UC 1Enter First Name");
+            string firstName = Console.ReadLine();
+            Program p = new Program();
+
+            p.isMatchFirstName(firstName);
+
+        }
+        //UC 1 First Name
+        public string isMatchFirstName(string firstName)
+        {
+            try
+            {
+                if (Regex.Match(firstName, "^[A-Z][a-z]{3,}$").Success)
+                {
+                    Console.WriteLine("Success , Firt Name is : " + firstName);
+                }
+                else
+                {
+                    Console.WriteLine("Failed");
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("First Name should Start with Upper Case Letter and minimum charecters should be 3 ");
+            }
+            return firstName;
+
+        }
+
+    }
+
+}
