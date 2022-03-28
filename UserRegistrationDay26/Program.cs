f4651aff4b9739d456c1900cbdcfb393117ade91
ManojMichaelX0 <manojthiparapu@gmail.com>
2022-03-28 21:37:19 +0530

    added Uc 13 Problem

--- UserRegistrationDay26/Program.cs
+++ UserRegistrationDay26/Program.cs
@@ -4,77 +4,17 @@ using System.Text.RegularExpressions;
 
 namespace UserRegistrationDay26
 {
-    public class Program : Exception
+    public class Program
     {
-        public static string firstName, lastName, email, mobileNumber, password;
-        public enum ExceptionType
-        {
-            Invalid_FirstName, Invalid_LastName, Invalid_Email, Invalid_MobileNumber, Invalid_Password
-        }
-        private readonly ExceptionType type;
-
         public static void Main(string[] args)
         {
-            Console.WriteLine("User Registration");
-            //UC 13
-            AddRecords();
            
-
-        }
-        public static void AddRecords()
-        {
-            Console.WriteLine("Enter First Name, Last Name,Email, MobileNumber,Password");
-            string firstname=Console.ReadLine();
-            string lastname=Console.ReadLine();
-            string email=Console.ReadLine();
-            string mobileNumber=Console.ReadLine();
-            string password=Console.ReadLine();
-            Console.WriteLine("First Name Check");
-            Check(firstname);
-            Console.WriteLine("Last Name Check");
-            Check(lastname);
-            Console.WriteLine("Email Check");
-            Check(email);
-            Console.WriteLine("Mobile Check");
-            Check(mobileNumber);
-            Console.WriteLine("Password Check");
-            Check(password);
-
-
-            void Check(string value)
-            {
-                Regex firstAndLastName = new Regex("^[A-Z][a-z]{3,}$");
-                Regex emailcheck = new Regex(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
-                Regex mobileNumbercheck = new Regex(@"^([0-9]{2})\s([0-9]{10})$");
-                Regex passwordCheck = new Regex(@"^([A-Za-z]{7,})([`~!@#$%^&*()_+=:;.,'<>/|\?]{1})([0-9]{1,})$");
-                if (firstAndLastName.Match(value).Success)
-                {
-                    Console.WriteLine("Success");
-                }
-                else if (firstAndLastName.Match(value).Success)
-                {
-                    Console.WriteLine("Success");
-                }
-                else if (emailcheck.Match(value).Success)
-                {
-                    Console.WriteLine("Success");
-                }
-                else if (mobileNumbercheck.Match(value).Success)
-                {
-                    Console.WriteLine("Success");
-                }
-                else if (passwordCheck.Match(value).Success)
-                {
-                    Console.WriteLine("Success");
-                }
-                else
-                {
-                    Console.WriteLine("Failed");
-                }
-            }
+            //UC 13
+            Console.WriteLine("User Registration Validating using Regular Expression!");
+            RegexSample regexSample = new RegexSample();
+            regexSample.Validating();
           
         }
-
 
 
     }
--- UserRegistrationTest/UserRegistrationTest.cs
+++ UserRegistrationTest/UserRegistrationTest.cs
@@ -1,84 +1,75 @@
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using UserRegistrationDay26;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistrationDay26;
 
-//namespace UserRegistrationTest
-//{
-//    //UC 10
-//    [TestClass]
-//    public class UserRegistrationTest
-//    {
-//        //UC 12
-//        [TestMethod]
-//        [DataRow("Manoj")]//Valid First Name
-//        [DataRow("manoj")]//InValid First Name
-//        public void FirstNameCheck(string fname)
-//        {
-//            try
-//            {
-//               bool value = Program.FirstNameCheck(fname);
-//            }
-//            catch (Program e)
-//            {
-//                Assert.AreEqual("Invalid First Name", e.Message);
-//            }
-//        }
-//        [TestMethod]
-//        [DataRow("Thiparapu")]//Valid Last Name
-//        [DataRow("thiparpu")]//InValid Last Name
-//        public void Given_Invalid_Last_Name(string lname)
-//        {
-//            try
-//            {
-//                bool value = Program.LastNameCheck(lname);
-//            }
-//            catch (Program e)
-//            {
-//                Assert.AreEqual("Invalid Last Name", e.Message);
-//            }
-//        }
-//        [TestMethod]
-//        [DataRow("manoj@gmail.com")]//Valid Email
-//        [DataRow("thiparpu")]//InValid Email
-//        public void Given_Invalid_Email(string mail)
-//        {
-//            try
-//            {
-//                bool value = Program.LastNameCheck(mail);
-//            }
-//            catch (Program e)
-//            {
-//                Assert.AreEqual("Invalid Email", e.Message);
-//            }
-//        }
-//        [TestMethod]
-//        [DataRow("91 8106529025")]//Valid Mobile Number
-//        [DataRow("801878912")]//InValid Mobile Number
-//        public void Given_Invalid_MobileNumber(string number)
-//        {
-//            try
-//            {
-//                bool value = Program.LastNameCheck(number);
-//            }
-//            catch (Program e)
-//            {
-//                Assert.AreEqual("Invalid Mobile Number", e.Message);
-//            }
-//        }
-//        [TestMethod]
-//        [DataRow("Password.4")]//Valid Password
-//        [DataRow("password")]//InValid Password
-//        public void Given_Invalid_Password(string pass)
-//        {
-//            try
-//            {
-//                bool value = Program.LastNameCheck(pass);
-//            }
-//            catch (Program e)
-//            {
-//                Assert.AreEqual("Invalid PassWord", e.Message);
-//            }
-//        }
+namespace UserRegistrationTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //validation for first Name
+        [TestMethod]
+        [DataRow("Manoj","Manoj")]//Test will Pass
+        [DataRow("manoj","Manoj")]//Test will Pass
 
-//    }
-//}
+        public void ValidateFirstname(string a, string expected)
+        {
 
+            var actual = RegexSample.ValidatingFirstName(a);
+            Assert.AreEqual(expected, actual);
+        }
+        //Validating for Last Name
+        [TestMethod]
+        [DataRow("Thiparapu","Thiparapu")]//Test will Pass
+        [DataRow("thiparapu","Thiparapu")]//Test will Pass
+
+        public void ValidateUserLastname(string a, string expected)
+        {
+            var actual = RegexSample.ValidatingLastName(a);
+            Assert.AreEqual(expected, actual);
+        }
+        //Validation for Email
+        [TestMethod]
+        [DataRow("abc@gmail.com", "abc@gmail.com")]
+        [DataRow("abc-100@yahoo.com", "abc-100@yahoo.com")]
+        [DataRow("abc.100@yahoo.com", "abc.100@yahoo.com")]
+        [DataRow("abc111@yahoo.com", "abc111@yahoo.com")]
+        [DataRow("abc111@abc.com", "abc111@abc.com")]
+        [DataRow("abc-100@abc.net", "abc-100@abc.net")]
+        [DataRow("abc.100@abc.com.au", "abc.100@abc.com.au")]
+        
+
+        public void ValidateUserEmail(string a, string expected)
+        {
+            var actual = RegexSample.ValidatingEmailId(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Validation for Phone Number
+        [TestMethod]
+   
+        [DataRow("91 8106529025","91 8106529025")]//Test Will Pass
+        [DataRow("8106529025","(91 8106529025")]//Test will Fail
+        
+        public void ValidateUserPhoneNumber(string a, string expected)
+        {
+            var actual = RegexSample.ValidatingPhoneNum(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+        ////Validation for Password
+        [TestMethod]
+        [DataRow("Manoj@123", "Manoj@123")]//Will Pass
+        [DataRow("Man#it123", "Man#lini123")]//Will Pass
+        [DataRow("Man@oj1A ", "Manoj1A")]//Will Fail
+       
+
+        public void ValidateUserPassword(string a, string expected)
+        {
+            var actual = RegexSample.ValidatingPassWord(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+    }
+}
